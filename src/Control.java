9039bb86b519216f3fd70c0598b2633378fb22e7
BFrederickss <malarakonrad05@gmail.com>
2025-09-15 00:21:17 +0100

    basic stuff

---
+++ src/Control.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Control {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GUI().setVisible(true);
+            }
+
+        });
+
+
+
+    }
+
+}
